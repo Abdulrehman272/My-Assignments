02cfe43ab6fca5d6d4646cdc0512a3070bfecf5a
Abdul Rehman <decentzidi72@gmail.com>
2023-04-05 12:19:59 -0700

    file

--- Assignment-10/Function-Handling/Question-23.js
+++ Assignment-10/Function-Handling/Question-23.js
@@ -1,1 +1,16 @@
-Write a JavaScript program to find sum of elements of array using function.
+// Write a JavaScript program to find sum of elements of array using function.
+
+
+
+function arraySum(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+  
+  var myArray = [2, 4, 6, 8, 10];
+  var result = arraySum(myArray);
+  console.log(result); // Output: 30
+  
--- Assignment-10/Function-Handling/Question-24.js
+++ Assignment-10/Function-Handling/Question-24.js
@@ -1,2 +1,24 @@
 
-24. Write a JavaScript program to find maximum and minimum elements in array using function.
+// 24. Write a JavaScript program to find maximum and minimum elements in array using function.
+
+
+
+function findMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
+      } else if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    return [min, max];
+  }
+  
+  // Example usage:
+  let array = [10, 5, 25, 30, 15];
+  let [min, max] = findMinMax(array);
+  console.log(`Minimum element is: ${min}`);
+  console.log(`Maximum element is: ${max}`);
+  
